d5ce7b5524eb67da77b5be5e317bbe83c985b0d6
Vihaan9319 <ietankur.android@gmail.com>
2026-07-07 15:33:55 -0700

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,12 @@
+a = 5
+b =10
+c = 8
+d = 1
+e = c*(a+b)/d
+print("The value of c*(a+b)/d is", e)
+name = "snape"
+age = 1
+if name == "Alex" or name == "Goat" and age >=2:
+    print("Welcome!")
+else:
+    print("Goodbye")
---
+++ activity2.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the numerator"))
+num2 = float(input("Enter the denominator"))
+if num1 % num2 == 0:
+    print("The numerator is divisible by the denominator")
+else:
+    print("These numbers are not divisible")
